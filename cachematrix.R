74641c77e0985646704994caa9be027202e881b4
FraCardia <88782318+FraCardia@users.noreply.github.com>
2021-09-22 06:59:21 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+      ## x is invertible matrix
 
+        inv=NULL
+        set=function(y){
+        x <<- y
+        inv <<- NULL       }
+      get= function()x
+      setinv=function(inverse)
+     inv <<- inverse
+     getinv= function() inv
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
@@ -12,4 +22,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv=x$getinv()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    # in the other situation, we calculate the inverse with
+    mat.data=x$get()
+    inv=solve(mat.data,...)
+    # to set the value of inverse in the cache
+    x$setinv()
+   return(inv)
 }
